99af82265a7fc1726e86fd652f33f397d95f7e64
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2021-08-09 23:42:42 +1000

    Добавлены проверки при инициализации

--- src/Masonry.tsx
+++ src/Masonry.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useMemo, Children, isValidElement } from 'react';
 import { normalizeBreakpoints } from './utils';
-import { Breakpoints, NormalizedBreakpoints } from './types';
+import { Columns, Gap, Breakpoints, NormalizedBreakpoints } from './types';
 
 export type MasonryProps = React.PropsWithChildren<{
-  columns?: { [key: string]: number };
-  gap?: { [key: string]: number };
+  columns?: Columns;
+  gap?: Gap;
   breakpoints?: Breakpoints;
   style?: React.CSSProperties;
   className?: string;
@@ -21,11 +21,11 @@ export default function Masonry(props: MasonryProps) {
   } = props;
 
   const [width, setWidth] = useState(window.innerWidth);
-  const [normalizedBreakpoints, setNormalizedBreakpoints] = useState<NormalizedBreakpoints>(
-    normalizeBreakpoints(breakpoints)
+  const [normalizedBreakpoints, setNormalizedBreakpoints] = useState<NormalizedBreakpoints | null>(
+    null
   );
-  const [currentСolumns, setCurrentColumns] = useState(1);
-  const [currentGap, setCurrentGap] = useState(0);
+  const [currentСolumns, setCurrentColumns] = useState<number | null>(null);
+  const [currentGap, setCurrentGap] = useState<number | null>(null);
 
   // Записываем текущую ширину страницы
   useEffect(() => {
@@ -40,38 +40,47 @@ export default function Masonry(props: MasonryProps) {
     };
   }, []);
 
+  // Нормализуем точки останова
+  useEffect(() => {
+    setNormalizedBreakpoints(normalizeBreakpoints(breakpoints));
+  }, [breakpoints]);
+
   // Устанавливаем параметры в зависимости от текущей ширины страницы
   useEffect(() => {
-    const currentBreakpointIndex = normalizedBreakpoints.findIndex((item, index, array) => {
-      return width >= item[1] || index === array.length - 1;
-    });
+    if (normalizedBreakpoints !== null) {
+      const currentBreakpointIndex = normalizedBreakpoints.findIndex((item, index, array) => {
+        return width >= item[1] || index === array.length - 1;
+      });
 
-    const breakpointForColumns = normalizedBreakpoints.find((item, index) => {
-      return index >= currentBreakpointIndex && columns[item[0]];
-    });
+      const breakpointForColumns = normalizedBreakpoints.find((item, index) => {
+        return index >= currentBreakpointIndex && columns[item[0]] !== undefined;
+      });
 
-    if (breakpointForColumns) {
-      setCurrentColumns(columns[breakpointForColumns[0]]);
-    }
+      if (breakpointForColumns) {
+        setCurrentColumns(columns[breakpointForColumns[0]]);
+      }
 
-    const breakpointForGap = normalizedBreakpoints.find((item, index) => {
-      return index >= currentBreakpointIndex && gap[item[0]];
-    });
+      const breakpointForGap = normalizedBreakpoints.find((item, index) => {
+        return index >= currentBreakpointIndex && gap[item[0]] !== undefined;
+      });
 
-    if (breakpointForGap) {
-      setCurrentGap(gap[breakpointForGap[0]]);
+      if (breakpointForGap) {
+        setCurrentGap(gap[breakpointForGap[0]]);
+      }
     }
   }, [columns, gap, normalizedBreakpoints, width]);
 
   // Устанавливаем дочерние элементы поочереди в каждую колонку
   const content = useMemo(() => {
-    const content: JSX.Element[][] = Array.from({ length: currentСolumns }, () => []);
+    const content: JSX.Element[][] = Array.from({ length: currentСolumns || 0 }, () => []);
 
-    Children.forEach(children, (child, index) => {
-      if (child && isValidElement(child)) {
-        content[index % currentСolumns].push(child);
-      }
-    });
+    if (currentСolumns !== null) {
+      Children.forEach(children, (child, index) => {
+        if (child && isValidElement(child)) {
+          content[index % currentСolumns].push(child);
+        }
+      });
+    }
 
     return content;
   }, [children, currentСolumns]);
@@ -80,23 +89,25 @@ export default function Masonry(props: MasonryProps) {
 
   return (
     <div id={id} style={{ ...style, display: 'flex' }} className={className}>
-      {content.map((items, index) => (
-        <div
-          key={`${id}-column-${index + 1}`}
-          id={`${id}-column-${index + 1}`}
-          style={{ flex: 1, marginLeft: index > 0 ? currentGap : undefined }}
-        >
-          {items.map((item, index) => (
+      {currentСolumns !== null && currentGap !== null
+        ? content.map((items, index) => (
             <div
-              key={`${id}-item-${index + 1}`}
-              id={`${id}-item-${index + 1}`}
-              style={{ marginTop: index > 0 ? currentGap : undefined }}
+              key={`${id}-column-${index + 1}`}
+              id={`${id}-column-${index + 1}`}
+              style={{ flex: 1, marginLeft: index > 0 ? currentGap : undefined }}
             >
-              {item}
+              {items.map((item, index) => (
+                <div
+                  key={`${id}-item-${index + 1}`}
+                  id={`${id}-item-${index + 1}`}
+                  style={{ marginTop: index > 0 ? currentGap : undefined }}
+                >
+                  {item}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ))}
+          ))
+        : null}
     </div>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,7 @@
+export type Columns = { [key: string]: number };
+
+export type Gap = { [key: string]: number };
+
 export type Breakpoints = { [key: string]: number };
+
 export type NormalizedBreakpoints = [string, number][];
